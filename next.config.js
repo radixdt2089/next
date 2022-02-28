e2496ee8e0ce6d23009b8122e9ed959b9186ba02
ankur.chovatiya <ankur.chovatiya@radixweb.com>
2022-02-28 19:14:19 +0530

    add some configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  reactStrictMode: true ,
-  swcMinify: false 
+  // reactStrictMode: true ,
+  swcMinify: false ,
+  env : {
+    BASE_URL : 'https://strapi-server-blog.herokuapp.com',
+  },  
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
